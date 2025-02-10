f7ce2311528315360931785734e5141ca26d678b
khankhatarnak123 <khankhatarnak1998@gmail.com>
2025-02-10 17:35:45 +0530

    added resume

--- src/Data/data.jsx
+++ src/Data/data.jsx
@@ -11,7 +11,8 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 export const cvLink =
-  "https://drive.google.com/uc?export=download&id=1V976Y-It2xDWZYeEWmVfWD-EpiD0NO-f";
+  // "https://drive.google.com/uc?export=download&id=1V976Y-It2xDWZYeEWmVfWD-EpiD0NO-f";
+  "https://drive.google.com/uc?export=download&id=1ESXrOi0ntHcA4R9R3mMYBbHO3Wd5Nddb";
 
 export const skills = [
   { id: 1, name: "HTML", icon: <FaHtml5 color="#E34F26" size="2rem" /> },
